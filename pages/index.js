d3a4537713e3256d483aad6e814971c98facaf30
oscarddddddd <qianlidong0@gmail.com>
2023-12-04 00:00:14 -0600

    first commit

---
+++ pages/index.js
@@ -0,0 +1,48 @@
+
+import { useState } from 'react'
+import Link from 'next/link'
+import styles from './login.module.css'
+import {Button, Input} from 'antd'
+import Header from '../layout/layout'
+
+export default function Login() {
+
+    // const auth = getAuth()
+    
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [error, setError] = useState("")
+
+    // Functions
+    const login = (e, email, password) => {
+        e.preventDefault()
+        if (!email || !password) {
+            setError("All fields are required");
+            
+        }
+        
+    }
+
+    return (
+
+      // <form className={styles.body} onSubmit = {(e)=> login(e,email,password)}>
+      //    
+          
+      // </form>
+      <div>
+        <Header className = {styles.header}/>
+        <div className={styles.body}>
+          <h1 className= {styles.loginTitle}>Login</h1>
+          <Input className={styles.input} type="email" placeholder="School Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <Input className={styles.input} type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <Button type="primary" onClick={login}>LOGIN</Button>
+          <p>Need Signup?
+            <Link href="/signup">Signup</Link>
+            
+          </p>
+
+        </div>
+      </div>
+        
+    )
+}
---
+++ pages/signup.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import Link from 'next/link'
+import { Input, Button} from "antd";
+import styles from "./login.module.css"
+
+
+export default function Signup(){
+    const [cname, setCName] = useState("")
+    const [fname, setFName] = useState("")
+    const [lname, setLName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [error, setError] = useState("")
+
+    const signup = async(e)=>{
+        e.preventDefault()
+        if(!cname ||!fname ||!lname ||!email ||!password){
+            console.log("All fields are required")
+           
+        }
+        else{
+            let res = await fetch('http://localhost:4000/user/test')
+            let data = await res.json()
+            console.log(data)
+        }
+        console.log(cname,fname,lname,email,password)
+    }
+    return(
+        <div className={styles.signup}>
+            <h1>注册</h1>
+            <Input className={styles.input} type="text" placeholder="Username" onChange={(e)=>setLName(e.target.value)}></Input>
+            <Input className={styles.input} type="email" placeholder="Email" onChange={(e)=>setEmail(e.target.value)}></Input>
+            <Input className={styles.input} type="password" placeholder="Password" onChange={(e)=>setPassword(e.target.value)}></Input>
+            <Button className={styles.input} type="primary" size="large" onClick={signup}>Submit</Button>
+        </div>
+
+    )
+}
