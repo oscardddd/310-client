e1c76a847381f4b9611b4572585c81cd1f5f266d
Bruce Zhu <bruce.zhekang.zhu@gmail.com>
2023-12-05 20:26:38 -0600

    sell-item and router done
    update!

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,81 @@
-
-import { useState } from 'react'
-import Link from 'next/link'
-import styles from './login.module.css'
-import {Button, Input} from 'antd'
-import Header from '../layout/layout'
+import { useState } from "react";
+import Link from "next/link";
+import styles from "./login.module.css";
+import { Button, Input } from "antd";
+import { useRouter } from "next/router";
+import Header from "../layout/layout";
 
 export default function Login() {
+  // const auth = getAuth()
+  const router = useRouter();
 
-    // const auth = getAuth()
-    
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [error, setError] = useState("")
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-    // Functions
-    const login = (e, email, password) => {
-        e.preventDefault()
-        if (!email || !password) {
-            setError("All fields are required");
-            
+  // Functions
+  const login = async (e) => {
+    e.preventDefault();
+    if (!email || !password) {
+      setError("All fields are required");
+    } else {
+      let res = await fetch(
+        "https://ug627f5dha.execute-api.us-east-2.amazonaws.com/test1/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "/*/",
+            "User-Agent": "PostmanRuntime/7.35.0",
+          },
+          body: JSON.stringify({
+            email: email,
+            password: password,
+          }),
         }
-        
+      );
+
+      let data = await res.json();
+      console.log(data);
+      if (data.status == "success") {
+        const userid = data.userid;
+        router.push({
+          pathname: "/sell",
+          query: { userid: userid },
+        });
+      } else {
+        console.log("error");
+      }
     }
+  };
 
-    return (
-
-      // <form className={styles.body} onSubmit = {(e)=> login(e,email,password)}>
-      //    
-          
-      // </form>
-      <div>
-        <Header className = {styles.header}/>
-        <div className={styles.body}>
-          <h1 className= {styles.loginTitle}>Login</h1>
-          <Input className={styles.input} type="email" placeholder="School Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-          <Input className={styles.input} type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-          <Button type="primary" onClick={login}>LOGIN</Button>
-          <p>Need Signup?
-            <Link href="/signup">Signup</Link>
-            
-          </p>
-
-        </div>
+  return (
+    <div>
+      <Header className={styles.header} />
+      <div className={styles.body}>
+        <h1 className={styles.loginTitle}>Login</h1>
+        <Input
+          className={styles.input}
+          type="email"
+          placeholder="School Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          className={styles.input}
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button type="primary" onClick={login}>
+          LOGIN
+        </Button>
+        <p>
+          Need Signup?
+          <Link href="/signup">Signup</Link>
+        </p>
       </div>
-        
-    )
+    </div>
+  );
 }
---
+++ pages/sell.js
@@ -0,0 +1,117 @@
+import { useState } from "react";
+import { Modal, Input, Button } from "antd";
+import styles from "./login.module.css";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+export default function Signup() {
+  const [itemName, setItemName] = useState("");
+  const [startPrice, setPrice] = useState("");
+  const [description, setDescription] = useState("");
+  const [imageData, setImageData] = useState("");
+  const [imageFilename, setImageFilename] = useState("");
+  const [errorMessage, setErrorMessage] = useState(""); // To store error message
+  const [modalVisible, setModalVisible] = useState(false); // To show/hide modal
+
+  const [userid, setUserId] = useState("");
+  const router = useRouter();
+
+  useEffect(() => {
+    if (router.query.userid) {
+      setUserId(router.query.userid);
+      console.log(userid);
+    }
+  }, [router.query.userId]);
+
+  const handleImageChange = async (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImageFilename(file.name);
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImageData(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const sell = async (e) => {
+    e.preventDefault();
+    if (!itemName || !startPrice || !description || !imageData) {
+      console.log("All fields are required");
+    } else if (!userid) {
+      alert("Identification failed");
+    } else {
+      console.log(itemName, startPrice, description, imageFilename, imageData);
+      let res = await fetch(
+        "https://ug627f5dha.execute-api.us-east-2.amazonaws.com/test1/sell-item",
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            "User-Agent": "PostmanRuntime/7.35.0",
+          },
+          body: JSON.stringify({
+            userid: userid,
+            item_name: itemName,
+            start_price: startPrice,
+            description: description,
+            filename: imageFilename,
+            data_str: imageData,
+            ttl: 1,
+          }),
+        }
+      );
+      let data = await res.json();
+      console.log(data);
+      if (res.status === 200) {
+        alert("Successfully submitted");
+      } else {
+        console.log(res);
+        alert("Something went wrong, please try again.");
+      }
+    }
+  };
+
+  const handleOk = () => {
+    setModalVisible(false); // Hide modal window when OK button clicks
+    router.push("/signup");
+  };
+
+  return (
+    <div className={styles.signup}>
+      <h1>Please Fill In the Item Information</h1>
+      <Input
+        className={styles.input}
+        type="text"
+        placeholder="Enter Your Item Name"
+        onChange={(e) => setItemName(e.target.value)}
+      />
+      <Input
+        className={styles.input}
+        type="number"
+        placeholder="Enter Start Price"
+        onChange={(e) => setPrice(e.target.value)}
+      />
+      <Input
+        className={styles.input}
+        type="text"
+        placeholder="Enter Description"
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <input type="file" onChange={handleImageChange} accept="image/*" />
+      <Button
+        className={styles.input}
+        type="primary"
+        size="large"
+        onClick={sell}
+      >
+        Submit
+      </Button>
+      {errorMessage && <p>{errorMessage}</p>} {/* Display error message */}
+      <Modal title="Error" visible={modalVisible} onOk={handleOk}>
+        <p>{errorMessage}</p>
+      </Modal>
+    </div>
+  );
+}
